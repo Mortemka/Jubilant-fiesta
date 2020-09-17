882765e4627e021a81a0df6e922cf3597f400904
Vasilii Savchenko <fukc.651@gmail.com>
2020-09-17 23:45:20 +0500

    modified index.php

--- index.php
+++ index.php
@@ -1,7 +1,20 @@
-<?php 
-$asd
-$io = [1 2 3 4];
-$love =  "hello";
-echo love;
-?>
+<!DOCTYPE html>
+<html lang="en">
 
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+</head>
+
+<body>
+  <?php 
+  $asd
+  $io = [1 2 3 4];
+  $love =  "hello";
+  echo love;
+  ?>
+
+</body>
+
+</html>
